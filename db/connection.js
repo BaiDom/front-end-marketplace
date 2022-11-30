1b6ecc3ea21bad2b2268c1c2b42d60aa93f6b556
BaiDom <dombaily88@gmail.com>
2022-11-30 12:02:48 +0000

    pushing is the right thing to do??

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,18 @@
-const dbConfig = require('../knexfile');
-const db = require('knex')(dbConfig);
+const dbConfig = require("../knexfile");
+const db = require("knex")(dbConfig);
+const ENV = process.env.NODE_ENV || "development";
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+      }
+    : {};
+
+module.exports = new Pool(config);
 
 module.exports = db;
