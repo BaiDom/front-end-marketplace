083b12c40ea0480838ea54cb844621441495414d
BaiDom <dombaily88@gmail.com>
2022-12-01 10:32:16 +0000

    hghg

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,4 @@
 const dbConfig = require("../knexfile");
 const db = require("knex")(dbConfig);
-const ENV = process.env.NODE_ENV || "development";
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
-}
-
-const config =
-  ENV === "production"
-    ? {
-        connectionString: process.env.DATABASE_URL,
-      }
-    : {};
-
-module.exports = new Pool(config);
 
 module.exports = db;
